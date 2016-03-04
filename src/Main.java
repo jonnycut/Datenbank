58bf3dfb8b089c891b05d5e5b148586fb94e0d6f
KNapret <jonnycut@yahoo.de>
2016-03-04 10:16:38 +0100

    Datenbankanbindung

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import programm.Programm;
+
+/**
+ * Created by KNapret on 04.03.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        new Programm();
+
+    }
+}
